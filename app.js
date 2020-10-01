9d3bf389f51f85675dcff44491e523bb5c78ed26
Akhil Konduru <60151778+maverick1910@users.noreply.github.com>
2020-10-01 13:13:04 +0530

    App.js Updated

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ var collection = require('./collections/collection');
 
 
 
-var pdfMake = require('pdfmake/build/pdfmake.js');
-var pdfFonts = require('pdfmake/build/vfs_fonts.js');
+
 require('dotenv').config();
 
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 
 
 const app = express();
@@ -115,37 +114,3 @@ app.listen(3000, console.log("Server up at 3000"));
 
 
 
-// axios.get('https://api.covid19india.org/v4/data.json').then((res) => {
-//     data = JSON.parse(JSON.stringify(res.data["AP"]["districts"]));
-//     console.log(data[con].total.confirmed);
-
-// }).catch(function (error) {
-//     console.log("No res data from api")
-// })
-
-// app.use('/e-passgen', urlencodedParser, function (req, res) {
-//     console.log(data);
-
-//     console.log(req.body);
-//     if (data[con].total.confirmed >= data["Chittoor"].total.confirmed) {
-//         status = 1;
-//     }
-//     else { status = 0; }
-//     console.log(status);
-//     const pdfRoute = require('./routes/pdfmake')(status,req.body);
-//     app.use('/pdfMake', pdfRoute);
-//     res.send('welcome, ' + req.body.username)
-//   })
-
-
-// app.use('/api',(req,res )=>{
-
-
-//     console.log(data);
-//      res.json(data);});
-//      console.log(data);
-//  if(data.districts.Anantapur.total.confirmed>=data.districts.Chittoor.total.confirmed)
-//   {
-//     status=1;
-//     }
-//    else{   status=0;}
